fe45cbf06c6a55b96933ef776097b2f8a559feb9
pedro <pedro@DESKTOP-HAHF0EE>
2026-03-30 23:07:03 -0300

    Inicio do Exercicio Listar do Nelio

--- src/exercicios_lista/Main.java
+++ src/exercicios_lista/Main.java
@@ -1,10 +1,40 @@
 package exercicios_lista;
 
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("ola");
+		Scanner sc = new Scanner(System.in);
+		List<Entidades> list = new ArrayList<>();
+		
+		System.out.println("Quantas Pessaos Deseja Adicionar? ");
+		Integer n = sc.nextInt();
+		sc.next();
+		
+		for (int i=1; i<n;i++)
+		{
+			System.out.println("Digite o ID");
+			int id = sc.nextInt();
+			
+			System.out.println("Nome");
+			String nome = sc.nextLine();
+			
+			System.out.println("Salario");
+			double salario = sc.nextDouble();
+			
+			Entidades enti = new Entidades(id, nome, null);
+			list.add(enti);
+			
+		}
+		
+		System.out.println("Qual a Porcentagem que Deseja Aumentar?");
+		
+	sc.close();	
 	}
 
 }
