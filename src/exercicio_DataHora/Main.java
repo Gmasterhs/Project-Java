baa0e8e1b615e5937228df8964f7f3c583832e16
pedro <pedrorary@gmail.com>
2026-04-06 21:58:56 -0300

    Exercicio Data Hora

---
+++ src/exercicio_DataHora/Main.java
@@ -0,0 +1,28 @@
+package exercicio_DataHora;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        LocalDate d01 = LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now();
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate d05 = LocalDate.parse("20/07/2026", fmt1);
+
+
+        System.out.println("Data Local do Meu Sistema: " + d01);
+        System.out.println("Hora do Meu Sistema: " + d02);
+        System.out.println("Data GMT: " + d03);
+        System.out.println("Data Personalizada no formato 8601: " + d04);
+        System.out.println(d05);
+        System.out.println(d05.format(fmt1));
+    }
+}
