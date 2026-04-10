c47d5af2a5d21619c0be54b3cfc12df31a969008
pedro <pedrorary@gmail.com>
2026-04-09 22:08:54 -0300

    Exercicio Worker in process

--- src/exercicio_WorkerUML_01/entities/Department.java
+++ src/exercicio_WorkerUML_01/entities/Department.java
@@ -2,6 +2,16 @@ package exercicio_WorkerUML_01.entities;
 
 public class Department
 {
-    //Departamento
-    String name;
+    private String name;
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/exercicio_WorkerUML_01/entities/HourContract.java
+++ src/exercicio_WorkerUML_01/entities/HourContract.java
@@ -1,6 +1,36 @@
 package exercicio_WorkerUML_01.entities;
 
-public class HourContract
+import java.time.LocalDate;
+
+public class HourContract   //Contrato por Hora
 {
-    //Contrato por Hora
+    private LocalDate date;
+    private Double valuePerHur;
+    private Integer hours;
+
+    //Contructors
+
+    public HourContract() {
+
+    }
+    public HourContract(LocalDate date) {
+        this.date = date;
+    }
+
+    //Getsrs and Setters
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    //Metods
+
+    public Double totalVolue ()
+    {
+        return valuePerHur * hours;
+    }
 }
