f156807a81413726e0a4790ac238416153e3c3d8
pedro <pedrorary@gmail.com>
2026-05-18 21:43:53 -0300

    Update 1.2

--- src/exercicio_Contribuinte/entities/Company.java
+++ src/exercicio_Contribuinte/entities/Company.java
@@ -1,5 +1,40 @@
 package exercicio_Contribuinte.entities;
 
-public class Company
+public class Company extends TaxPayment
 {
+    private int namberOfEmployees;
+
+    //Getts and Setters
+
+
+    public int getNamberOfEmployees() {
+        return namberOfEmployees;
+    }
+
+    public void setNamberOfEmployees(int namberOfEmployees) {
+        this.namberOfEmployees = namberOfEmployees;
+    }
+
+    //Constructor
+
+    public Company(String name, Double anualIncome, int namberOfEmployees) {
+        super(name, anualIncome);
+        this.namberOfEmployees = namberOfEmployees;
+    }
+
+    //Methods
+
+
+    @Override
+    public Double tax() {
+        if (namberOfEmployees <= 9) {
+            return (getAnualIncome() * 0.16);
+
+        }
+        else
+        {
+            return (getAnualIncome() * 0.14);
+        }
+
+    }
 }
--- src/exercicio_Contribuinte/entities/Individual.java
+++ src/exercicio_Contribuinte/entities/Individual.java
@@ -1,6 +1,46 @@
 package exercicio_Contribuinte.entities;
 
-public class Individual
+public class Individual extends TaxPayment
 {
+    private Double healthExpenditure;
 
+    //Gettsrs and Setteres
+
+
+    public Double getHealthExpenditure() {
+        return healthExpenditure;
+    }
+
+    public void setHealthExpenditure(Double healthExpenditure) {
+        this.healthExpenditure = healthExpenditure;
+    }
+
+    //Constructor
+
+    public Individual(String name, Double anualIncome, Double healthExpenditure) {
+        super(name, anualIncome);
+        this.healthExpenditure = healthExpenditure;
+    }
+
+    // Methods
+
+
+    @Override
+    public Double tax()
+    {
+        double imp;
+        if (healthExpenditure <= 20000.0)
+        {
+            imp = getAnualIncome() * 0.15;
+        }
+        else
+        {
+            imp =  getAnualIncome() * 0.25;
+        }
+
+        double res = imp - (healthExpenditure * 0.5);
+        return  res;
+
+
+    }
 }
