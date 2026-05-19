cdfc3186b879fb6e362a42e9ceb755e917b03cd4
Pedro <pedrorary@gmail.com>
2026-05-19 09:56:42 -0300

    update 1.3

--- src/exercicio_Contribuinte/Main.java
+++ src/exercicio_Contribuinte/Main.java
@@ -1,9 +1,30 @@
 package exercicio_Contribuinte;
 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of tax payers:");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++)
+        {
+            System.out.println("Taxpayer " + "#" + (i  + 1) + " data:");
+            System.out.println("Individual or company(i/c)?");
+            char esc = sc.next().charAt(0);
+            System.out.println("Name:");
+            String name = sc.nextLine();
+            sc.next();
+            System.out.println("Anual income: ");
+            double anualIncome = sc.nextDouble();
+            System.out.println("Health expenditures: ");
+            double healthExpenditures = sc.nextDouble();
+
+        }
 
     }
 }
