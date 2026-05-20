3b546678e06cbd4e8f2b80b4d0cb5451e9a7f166
Pedro <pedrorary@gmail.com>
2026-05-20 11:44:05 -0300

    Update 1.0

--- src/exercicio_SistemaCombate/Main.java
+++ src/exercicio_SistemaCombate/Main.java
@@ -1,4 +1,38 @@
 package exercicio_SistemaCombate;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main
+{
+    static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List <Character> list = new ArrayList<>();
+
+        System.out.println("Enter the number of characters: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n ; i++) {
+
+            System.out.println("Character # " +  (i + 1) + " data: ");
+            System.out.println("Melee or Mystic (m/y)?");
+            char op = sc.next().charAt(0);
+            sc.nextLine();
+
+            System.out.println("Name: ");
+            String name = sc.nextLine();
+
+
+            if (op == 'm') {
+                System.out.println("Weapon Bonus:");
+            }
+
+        }
+
+    }
 }
