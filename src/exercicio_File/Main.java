8f85c6920b168b428abe70ccd42e6fd446d44dda
pedro <pedrorary@gmail.com>
2026-07-15 22:52:47 -0300

    att

--- src/exercicio_File/Main.java
+++ src/exercicio_File/Main.java
@@ -1,6 +1,6 @@
 package exercicio_File;
 
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main
@@ -14,9 +14,47 @@ public class Main
 
         File file = new File(caminho);
 
-        String pastaPai = file.getParent();
+        String pastaPai = file.getParent(); //obtem o caminho despresando o nome do arquivo
+        String novoCaminho = pastaPai + "\\out";
 
-        String cnovoCaminho = pastaPai + "\\out";
+        File novaPasta = new File(novoCaminho);
+        novaPasta.mkdir(); // cria novo diretorio
+
+        String arquivoFinal = novoCaminho + "\\summary.csv";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(caminho)) ;
+             BufferedWriter bw = new BufferedWriter(new FileWriter(arquivoFinal)))
+        {
+            String line = br.readLine(); // ler a proxima linha e se não achar nada retorna null
+
+            while (line != null)
+            {
+                System.out.println(line);
+                String [] campo = line.split(","); //usa um marcador para recortar uma String
+
+                String nome = campo[0];
+                double preco = Double.parseDouble(campo[1]);
+                int quantidade = Integer.parseInt(campo[2]);
+
+                double total = preco * quantidade;
+
+                bw.write(nome + "," + String.format("%.2f",total));
+                bw.newLine();
+
+                line = br.readLine();
+            }
+
+            System.out.println("Salvo com Sucesso");
+
+
+
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error " + e.getMessage() );
+        } catch (IOException e) {
+            System.out.println("Error " + e.getMessage() );
+        }
+
 
     }
 }
