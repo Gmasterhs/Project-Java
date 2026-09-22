7d3f941206302ee71af82d2e1cb7184b5f88621a
pedro <pedrorary@gmail.com>
2026-09-22 18:55:11 -0300

    Exemplo de Generics

--- src/TipoGenerico/Main.java
+++ src/TipoGenerico/Main.java
@@ -1,9 +1,25 @@
 package TipoGenerico;
 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main()
     {
-        System.out.println("ola");
+        Scanner sc = new Scanner(System.in);
+
+        PrintService ps = new PrintService();
+        int n =  sc.nextInt();
+
+        for (int i = 0; i < n; i++)
+        {
+            int valor = sc.nextInt();
+            ps.addVolue(valor);
+        }
+
+        ps.print();
+        System.out.println("Primeiro: " + ps.first());
+
+
     }
 }
